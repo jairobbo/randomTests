f40792e8578459077172ddc6a5f8edd199182bf4
Jairo Bambang Oetomo <jbambangoetomo@nlmb000063.nl.deloitte.com>
2018-06-06 15:00:02 +0200

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Bambang Oetomo, Jairo (NL - Amsterdam) on 06/06/2018.
+//  Copyright © 2018 Bambang Oetomo, Jairo (NL - Amsterdam). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
